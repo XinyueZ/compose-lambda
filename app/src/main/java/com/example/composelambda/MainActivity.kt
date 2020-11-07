cde198fdff75b38f38638ad314f6eb00f75b904a
XinyueZ <dev.xinyue.zhao@gmail.com>
2020-11-07 13:35:56 +0100

    try: image, resource

--- app/src/main/java/com/example/composelambda/MainActivity.kt
+++ app/src/main/java/com/example/composelambda/MainActivity.kt
@@ -3,38 +3,53 @@ package com.example.composelambda
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.Context
+import androidx.ui.core.Alignment
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
-import androidx.ui.layout.Column
-import androidx.ui.layout.MainAxisAlignment
-import androidx.ui.layout.Row
-import androidx.ui.layout.Spacing
+import androidx.ui.foundation.DrawImage
+import androidx.ui.graphics.Image
+import androidx.ui.graphics.imageFromResource
+import androidx.ui.layout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GetListOfText()
+            BuildFrontPage(this)
         }
     }
 }
 
 @Composable
-fun GetListOfText() {
+fun BuildFrontPage(context: Context) {
+    val image: Image = imageFromResource(context.resources, R.drawable.trump_dump)
     Column(
-        mainAxisAlignment = MainAxisAlignment.SpaceBetween,
     ) {
-        Row(
-            mainAxisAlignment = MainAxisAlignment.SpaceBetween,
-            modifier = Spacing(all = 10.dp),
-        )
-        {
-            Text(text = "Hello, compose 1.1")
-            Text(text = "Hello, compose 1.2")
-            Text(text = "Hello, compose 1.1")
+        Container(
+            expanded = true,
+            height = 300.dp,
+            padding = EdgeInsets(all = 10.dp),
+        ) {
+            DrawImage(image)
         }
-        Text(text = "Hello, compose 2")
-        Text(text = "Hello, compose 3")
+        Container(
+            expanded = true,
+            alignment = Alignment.Center,
+        ) {
+            Row(
+                modifier = Spacing(top = 20.dp, bottom = 20.dp),
+            )
+            {
+                Text(text = "This guy is occupying the Oval office.")
+            }
+        }
+        Container(
+            alignment = Alignment.Center,
+            padding = EdgeInsets(left = 10.dp, right = 10.dp),
+        ) {
+            Text(text = "The former president of U.S is currently occupying the Oval office, although the president elected Biden has won the 2020 Pres election.")
+        }
     }
 }
