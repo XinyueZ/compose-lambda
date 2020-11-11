f7d4986511cbfb9fd111065de2ed4106718ae76f
XinyueZ <dev.xinyue.zhao@gmail.com>
2020-11-11 15:32:06 +0100

    Simply compose with Crossfade

--- android/app/src/main/java/com/example/composelambda/appNav/AppRouter.kt
+++ android/app/src/main/java/com/example/composelambda/appNav/AppRouter.kt
@@ -1,10 +1,12 @@
 package com.example.composelambda.appNav
 
 import android.util.Log
+import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.composelambda.pages.BuildDetailPage
 import com.example.composelambda.pages.BuildOverviewPage
@@ -22,19 +24,19 @@ fun NavigationContent(child: @Composable () -> Unit) {
 fun AppRouter() {
     NavHost(AppNavigator, startDestination = ROOT) {
         composable(ROOT) {
-//            Crossfade(AppNavigator.currentBackStackEntryAsState()) {
-            BuildOverviewPage()
-//            }
+            Crossfade(AppNavigator.currentBackStackEntryAsState()) {
+                BuildOverviewPage()
+            }
         }
         composable(OVERVIEW) {
-//            Crossfade(AppNavigator.currentBackStackEntryAsState()) {
-            BuildOverviewPage()
-//            }
+            Crossfade(AppNavigator.currentBackStackEntryAsState()) {
+                BuildOverviewPage()
+            }
         }
         composable(DETAIL) {
-//            Crossfade(AppNavigator.currentBackStackEntryAsState()) {
-            BuildDetailPage()
-//            }
+            Crossfade(AppNavigator.currentBackStackEntryAsState()) {
+                BuildDetailPage()
+            }
         }
     }
 
