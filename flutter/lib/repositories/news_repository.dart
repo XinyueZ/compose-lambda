2329b390d521ad83f7dc64609266c75c099a7149
XinyueZ <dev.xinyue.zhao@gmail.com>
2020-12-05 21:06:09 +0100

    Flutter, add api for premium-news on repo, PREFERENCES for router

--- flutter/lib/repositories/news_repository.dart
+++ flutter/lib/repositories/news_repository.dart
@@ -4,9 +4,11 @@ import 'dart:io';
 import 'package:async/async.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_compose_lambda/domains/breaking_news.dart';
+import 'package:flutter_compose_lambda/domains/premium_news.dart';
 import 'package:flutter_compose_lambda/network/rest_client.dart';
 
 abstract class NewsRepository {
+  Future<Result<PremiumNews>> fetchPremiumNews();
   Future<Result<BreakingNews>> fetchBreakingNews();
 }
 
@@ -40,5 +42,29 @@ class NewsRepositoryImpl implements NewsRepository {
 
     final jsonBox = jsonDecode(result.body) as Map<String, dynamic>;
     return Result.value(BreakingNews.fromJson(jsonBox));
+  }
+
+  @override
+  Future<Result<PremiumNews>> fetchPremiumNews() async {
+    final result =
+        await _restClient.get("/s/ibjhxji5o237497/premium-news.json");
+
+    if (result.statusCode != HttpStatus.ok) {
+      throw Exception(
+          "Failed to get answer from Telematics API. Status code: ${result.statusCode}. Body: ${result.body}");
+    }
+
+    final now = DateTime.now().millisecondsSinceEpoch;
+
+    if (now % 3 == 0) {
+      return Result.error(Exception("Cannot fetch data."));
+    }
+
+    if (now % 5 == 0) {
+      throw Exception("Fatal issue!");
+    }
+
+    final jsonBox = jsonDecode(result.body) as Map<String, dynamic>;
+    return Result.value(PremiumNews.fromJson(jsonBox));
   }
 }
